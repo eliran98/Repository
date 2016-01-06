a89824051ea14bbdc6fbfcb0d381ee615fac5978
eliran98 <elirann.atias@gmail.com>
2016-01-06 18:18:06 +0200

    add

---
+++ myTestProject/TestProject/src/com/testproject/algorithm/BinerySearch.java
@@ -0,0 +1,41 @@
+package com.testproject.algorithm;
+/*Given a sorted array arr[] of n elements, write a function to search a given element x in arr[]
+ * TimeComplexity o(logn).
+ */
+public class BinerySearch {
+   
+	public static int searchRecur(int[] arr , int start,int end,int searchElement){
+		
+		if(end>=start){
+			int mid = (end - start)/2;
+			if(arr[mid] == searchElement){
+				return mid;
+			}
+			
+			if(arr[mid]<searchElement){
+				return searchRecur(arr,mid+1,end,searchElement);
+			}else{
+				return searchRecur(arr,start,mid-1,searchElement);
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static int searchIteration(int[] arr , int start,int end,int searchElement){
+		
+		while(end>=start){
+			int mid = (end - start)/2;
+			if(arr[mid] == searchElement){
+				return mid;
+			}
+			if(arr[mid]<searchElement){
+				start = mid + 1;
+			}else{
+				end = mid - 1;
+			}
+		}
+		
+		return -1;
+	}
+}
